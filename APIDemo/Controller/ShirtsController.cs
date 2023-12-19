5e30cb95bda188aa2a1aebf797e53ed87a22d0ed
Khoa <ankhoa.dev@gmail.com>
2023-12-19 23:32:13 +0700

    add Routing

---
+++ APIDemo/Controller/ShirtsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIDemo.Controller
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt: {id}";
+        }
+        [HttpPost]
+        public string CreateShirt()
+        {
+            return "Create shirt";
+        }
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt: {id}";
+        }
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting shirt: {id}";
+        }
+    }
+}
