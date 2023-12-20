87171d22c6d52862571b0775e1b9b85ab17e37e1
Khoa <ankhoa.dev@gmail.com>
2023-12-20 17:51:11 +0700

    Return type

--- APIDemo/Controller/ShirtsController.cs
+++ APIDemo/Controller/ShirtsController.cs
@@ -1,4 +1,5 @@
 ﻿using APIDemo.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIDemo.Controller
@@ -7,31 +8,40 @@ namespace APIDemo.Controller
     [Route("api/[Controller]")]
     public class ShirtsController : ControllerBase
     {
-        [HttpGet]
-        public string GetShirts()
+        private List<Shirt> shirts = new List<Shirt>()
         {
-            return "Reading all the shirts";
+            new Shirt{ShirtId = 1, Brand = "My Brand", Color = "Red", Gender = "Men", Price = 30, Size = 10},
+            new Shirt{ShirtId = 2, Brand = "My Brand", Color = "Blue", Gender = "Men", Price = 45, Size = 42},
+            new Shirt{ShirtId = 3, Brand = "My Brand", Color = "White", Gender = "Men", Price = 61, Size = 55},
+            new Shirt{ShirtId = 4, Brand = "My Brand", Color = "Black", Gender = "Men", Price = 22, Size = 33}
+        };
+        [HttpGet]
+        public IActionResult GetShirts()
+        {
+            return Ok("Reading all the shirts");
         }
 
         [HttpGet("{id}")]
-        public string GetShirtById(int id)
+        public IActionResult GetShirtById(int id)
         {
-            return $"Reading shirt: {id}";
+            var shirt = shirts.FirstOrDefault(x => x.ShirtId == id);
+            if(shirt == null) return NotFound();
+            return Ok(shirt);
         }
         [HttpPost]
-        public string CreateShirt([FromBody] Shirt shirt)
+        public IActionResult CreateShirt([FromBody] Shirt shirt)
         {
-            return "Create shirt";
+            return Ok("Create shirt");
         }
         [HttpPut("{id}")]
-        public string UpdateShirt(int id)
+        public IActionResult UpdateShirt(int id)
         {
-            return $"Updating shirt: {id}";
+            return Ok($"Updating shirt: {id}");
         }
         [HttpDelete("{id}")]
-        public string DeleteShirt(int id)
+        public IActionResult DeleteShirt(int id)
         {
-            return $"Deleting shirt: {id}";
+            return Ok($"Deleting shirt: {id}");
         }
     }
 }
