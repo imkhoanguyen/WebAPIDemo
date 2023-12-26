2d83a2fc656ab70f40bc503da8e7510836fc8d20
Khoa <ankhoa.dev@gmail.com>
2023-12-26 19:05:19 +0700

    Migration

--- APIDemo/Data/ApplicationDbContext.cs
+++ APIDemo/Data/ApplicationDbContext.cs
@@ -6,6 +6,12 @@ namespace APIDemo.Data
 {
     public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
         public DbSet<Shirt> Shirts { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- APIDemo/Program.cs
+++ APIDemo/Program.cs
@@ -1,7 +1,16 @@
+using APIDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ShirtStoreManagement"));
+}
+);
 
 var app = builder.Build();
 
